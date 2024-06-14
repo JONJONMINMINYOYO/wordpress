dbda2ec9a11d8cd90d22e5166662b0251c6292df
steve <withliterature@gmail.com>
2024-06-14 23:19:37 +0900

    define( 'DB_NAME', 'wd-php' );
    define( 'DB_NAME', 'wd-php' );

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress-php' );
+define( 'DB_NAME', 'wd-php' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
